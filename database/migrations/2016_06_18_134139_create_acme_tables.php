d8448c41692b96c1387bbad8fc29c436eb0920fe
metaclassing <metaclassing@secureobscure.com>
2016-07-03 14:35:18 -0500

    added migrations and database crap

--- database/migrations/2016_06_18_134139_create_acme_tables.php
+++ database/migrations/2016_06_18_134139_create_acme_tables.php
@@ -12,13 +12,15 @@ class CreateAcmeTables extends Migration
      */
     public function up()
     {
-        Schema::create('accounts', function (Blueprint $table) {
+        Schema::create('acme_accounts', function (Blueprint $table) {
             $table->increments('id');
 			$table->string('name');				// simple name to reference the account by
 			$table->string('contact');			// email address for acme ca contact information
-			$table->longtext('zones');			// list of authorized top level domains for this account to use
+			$table->longtext('zones');			// list of authorized top level zones for this account to use
 			$table->longtext('publickey');		// ascii armor public key pem format
 			$table->longtext('privatekey');		// ascii armor private key pem format
+			$table->string('acmecaurl');		// url for acme ca api to send clients to
+			$table->string('acmelicense');		// url for acme ca license for account registration
 			$table->string('authtype');			// dns-01 is the only option supported
 			$table->string('authprovider');		// cloudflare or verisign
 			$table->string('authuser');			// cloudflare email address or verisign username
@@ -29,12 +31,11 @@ class CreateAcmeTables extends Migration
             $table->softDeletes();          // keep deactivated certificates in the table
         });
 
-        Schema::create('certificates', function (Blueprint $table) {
+        Schema::create('acme_certificates', function (Blueprint $table) {
             $table->increments('id');
-
 			$table->integer('account_id')->unsigned();
             $table->string('name');			// simple name for the certificate
-            $table->longtext('domains');	// json array of subject alternative names
+            $table->longtext('subjects');	// whitespace delimited list of subject alternative names
             $table->longtext('publickey');	// pem public key
             $table->longtext('privatekey');	// pem private key
             $table->longtext('request');	// pem certificate signing request
@@ -42,29 +43,13 @@ class CreateAcmeTables extends Migration
             $table->longtext('chain');		// pem chain of intermediate CA
 			$table->dateTime('expires');	// datetime for expiration time
             $table->string('status');		// status of the signing request
-			$table->longtext('response');	// dump of the last signing request response
             $table->timestamps();
             $table->softDeletes();          // keep deactivated certificates in the table
 
             // 1:many account->certificates relationship
-            $table->foreign('account_id')->references('id')->on('accounts');
+            $table->foreign('account_id')->references('id')->on('acme_accounts');
         });
 
-		// I dont know if I need a table for this or fuckit we'll do it live
-		/*
-        Schema::create('challenges', function (Blueprint $table) {
-            $table->increments('id');
-
-            // 1:many certificate->challenges relationship
-            $table->foreign('certificate_id')->references('id')->on('certificates');
-
-            $table->string('one');
-            $table->string('two');
-            $table->string('three');
-            $table->string('four');
-            $table->timestamps();
-        });
-		*/
     }
 
     /**
@@ -74,8 +59,7 @@ class CreateAcmeTables extends Migration
      */
     public function down()
     {
-        //Schema::drop('challenges');
-        Schema::drop('certificates');
-        Schema::drop('accounts');
+        Schema::drop('acme_certificates');
+        Schema::drop('acme_accounts');
     }
 }
