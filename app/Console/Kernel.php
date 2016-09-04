1e48a876bb96507e43d038d9d313652370886860
metaclassing <metaclassing@secureobscure.com>
2016-09-04 16:36:22 -0500

    added php artisan acme:renew and ca:renew commands with some options

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,9 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        // Commands\Inspire::class,
+        Commands\Inspire::class,
+        Commands\Acme\Renew::class,
+        Commands\Ca\Renew::class,
     ];
 
     /**
