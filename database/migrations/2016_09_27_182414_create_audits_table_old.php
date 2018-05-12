bb11fe182d5fefd3435c4d7ec2daefa405cf765b
metaclassing <metaclassing@secureobscure.com>
2018-05-12 02:23:47 -0500

    double sigh

--- database/migrations/2016_09_27_182414_create_audits_table_old.php
+++ database/migrations/2016_09_27_182414_create_audits_table_old.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAuditsTable extends Migration
+class CreateAuditsTableOld extends Migration
 {
     /**
      * Run the migrations.
