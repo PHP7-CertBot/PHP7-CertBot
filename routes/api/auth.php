2e723dff1940e1f87570472d1b4adb14a2f820c9
root <root@azrcerwld001>
2018-05-15 12:14:39 -0500

    removed old docblock for route about to be removed

--- routes/api/auth.php
+++ routes/api/auth.php
@@ -10,31 +10,5 @@ $options = [
                                ],
            ];
 Route::group($options, function () {
-    /**
-     * @SWG\Post(
-     *     path="/api/authenticate",
-     *     tags={"Authentication"},
-     *     summary="Get JSON web token by LDAP user authentication",
-     *     @SWG\Parameter(
-     *         name="username",
-     *         in="formData",
-     *         description="LDAP username",
-     *         required=true,
-     *         type="string"
-     *     ),
-     *     @SWG\Parameter(
-     *         name="password",
-     *         in="formData",
-     *         description="LDAP password",
-     *         required=true,
-     *         type="string"
-     *     ),
-     *     @SWG\Response(
-     *         response=200,
-     *         description="Authentication succeeded",
-     *         ),
-     *     ),
-     * )
-     **/
     Route::post('', 'AuthController@authenticate');
 });
