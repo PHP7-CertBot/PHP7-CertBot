ccaef1e424f31b719c3505f621104daa8b26171f
root <root@azrcerwlp001>
2018-06-02 13:41:26 -0500

    added badges and fixed formatting for certbot view

--- resources/views/certbot.blade.php
+++ resources/views/certbot.blade.php
@@ -19,12 +19,12 @@
                 font-family: 'Lato';
             }
 
-			.band {
-				position: absolute;
-				top: 0;
-				right: 0;
-				border: 0;
-			}
+            .band {
+                position: absolute;
+                top: 0;
+                right: 0;
+                border: 0;
+            }
 
             .container {
                 text-align: center;
@@ -46,62 +46,66 @@
                 font-size: 32px;
             }
             .documentation a {
-				text-decoration: none;
-				color: blue;
+                text-decoration: none;
+                color: blue;
             }
             .documentation ul {
                 margin: 0px;
             }
 
-			.footer {
-			    position: absolute;
-			    width: 100%;
-			    left: 0px;
-			    bottom: 0px;
-			}
+            .footer {
+                position: absolute;
+                width: 100%;
+                left: 0px;
+                bottom: 0px;
+            }
 
-			.banner {
-				display: block;
-				text-align: center;
-			}
-			.banner img {
-				height: 50px;
-			}
+            .banner {
+                display: block;
+                text-align: center;
+            }
+            .banner img {
+                height: 50px;
+            }
         </style>
     </head>
     <body>
-		<div class="band">
-			<a href="https://github.com/metaclassing/PHP7-CertBot">
-			<img src="images/githubforkmegreen.png">
-			</a>
-		</div>
+        <div class="band">
+            <a href="https://github.com/metaclassing/PHP7-CertBot">
+            <img src="images/githubforkmegreen.png">
+            </a>
+        </div>
         <div class="container">
             <div class="content">
                 <div class="title">PHP7-CertBot</div>
                 <div class="documentation">
-					<ul>
+                    <img src="https://scrutinizer-ci.com/g/metaclassing/PHP7-CertBot/badges/build.png?b=master">
+                    <img src="https://styleci.io/repos/62511938/shield?branch=master">
+                    <img src="https://scrutinizer-ci.com/g/metaclassing/PHP7-CertBot/badges/coverage.png?b=master">
+                    <img src="https://scrutinizer-ci.com/g/metaclassing/PHP7-CertBot/badges/quality-score.png?b=master">
+                    <ul>
                         <li>
                             <a href="api/documentation/">API Documentation</a>
                         </li>
                         <li>
-					        <a href="monitor/">Certificate Expiration Monitor</a>
+                            <a href="monitor/">Certificate Expiration Monitor</a>
                         </li>
-					</ul>
-				</div>
+                    </ul>
+                </div>
             </div>
         </div>
-		<div class="footer">
-			<div class="banner">
-				<img src="images/letsencrypt.png">
-				<img src="images/lgplv3.png">
-				<img src="images/laravel.png">
-				<img src="images/jwtio.png">
-				<img src="images/phpseclib.png">
-				<img src="images/swagger.png">
-				<img src="images/nginx.png">
-				<img src="images/php7.png">
-				<img src="images/mysql.png">
-			</div>
-		</div>
+        <div class="footer">
+            <div class="banner">
+                <img src="images/letsencrypt.png">
+                <img src="images/lgplv3.png">
+                <img src="images/laravel.png">
+                <img src="images/jwtio.png">
+                <img src="images/phpseclib.png">
+                <img src="images/swagger.png">
+                <img src="images/nginx.png">
+                <img src="images/php7.png">
+                <img src="images/mysql.png">
+            </div>
+        </div>
     </body>
 </html>
